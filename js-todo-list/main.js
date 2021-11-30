805db1c2b577db4ec60017151dcf001d9ca13219
yuchan <lumir1031@gmail.com>
2021-11-30 15:20:31 +0800

    answer_complete

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,33 @@
 // TO DO
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('ul').addEventListener('click', function (e) {
+    if (e.target.nodeName === 'LI') {
+      e.target.classList.toggle('checked')
+    }
+
+    if (e.target.nodeName === 'SPAN') {
+      e.target.parentNode.remove()
+    }
+  })
+
+  document.querySelector('#input').addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+      document.querySelector('#addBtn').click()
+    }
+  })
+
+  document.querySelector('#addBtn').addEventListener('click', function() {
+    let content = document.querySelector("#input").value.trim()
+    if (content.length < 1) return
+    let li = document.createElement('li')
+    let span = document.createElement('span')
+    let ul = document.querySelector('ul')
+    span.appendChild(document.createTextNode('x'))
+    span.classList.add('close')
+    li.appendChild(document.createTextNode(content))
+    li.appendChild(span)
+    ul.appendChild(li)
+    document.querySelector("#input").value = ''
+  })
+})
